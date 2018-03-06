e52679e9bec02bfc2207064344b1d4b0ff79fb4f
Sam Anderson <samel.2710@gmail.com>
2018-03-06 23:28:11 +0000

    Cleanup before advanced arguments

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,9 +6,6 @@ const notes = require('./notes');
 
 const argv = yargs.argv;
 const command = argv._[0];
-
-console.log(`Command: ${command}`);
-console.log('Yargs: ', argv);
 
 if(command === 'add') {
 	const note = notes.addNote(argv.title, argv.body);
